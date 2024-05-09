7ad304109eecc719adc52abbc39d7668e61d30e2
MuhammadAhmad34 <m.ahmadamjad21@gmail.com>
2024-05-09 15:42:27 +0500

    Modified Delete, insert and Search Functionality

--- LinkedList.py
+++ LinkedList.py
@@ -3,20 +3,21 @@ class Node:
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
         self.count = 0
 
-    def __len__(self)->int:
+    def __len__(self) -> int:
         return self.count
-    
+
     def insertHead(self, data):
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
         self.count += 1
-    
+
     def insertTail(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -27,7 +28,7 @@ class LinkedList:
             curr = curr.next
         curr.next = new_node
         new_node.next = None
-        self.count += 1 
+        self.count += 1
 
     def insert(self, index, data):
         new_node = Node(data)
@@ -40,7 +41,7 @@ class LinkedList:
         new_node.next = curr.next
         curr.next = new_node
         self.count += 1
-    
+
     def pop(self):
         if self.head is None:  # Check for empty list
             return None  # Indicate nothing was popped
@@ -58,13 +59,65 @@ class LinkedList:
             prev.next = None  # Detach the last node
 
         self.count -= 1
-    
+
+    def Deletehead(self):
+
+        if self.head is None:
+            raise "Given List is Empty"
+
+        self.head = self.head.next
+        self.count -=1
+
+    def DeleteByVal(self, val):
+        if self.head is None:
+            raise "Given List is Empty"
+            return
+        curr = self.head
+
+        if self.head.data == val:
+            self.Deletehead()
+
+        while curr.next != None:
+            if curr.next.data == val:
+                break
+            curr = curr.next
+
+        if curr.next is None:
+            print("Not Found")
+            return
+        else:
+            curr.next = curr.next.next
+            self.count-=1
+
+    def SearchByVal(self, value):
+        if self.head is None:
+            raise "Empty List"
+        
+        while self.head.next != None:
+            if self.head.data == value:
+                print("Found")
+                return
+            self.head = self.head.next
+
+        print("Not Found")
+
+    def SearchIndex(self, index:int):
+        if self.head is None:
+            raise "List is empty"
+        if index < self.count:
+            raise IndexError("Index Out of Range....")
+        for i in range(index-1):
+            self.head = self.head.next
+        print(f"Value at given Index: {self.head.data}")
+
     def PrintList(self):
         curr = self.head
         while curr != None:
             print(f"{curr.data}->", end='')
             curr = curr.next
-        
+        print("NULL")
+
+
 if __name__ == "__main__":
     LIST = LinkedList()
     LIST.insertHead(50)
@@ -72,9 +125,4 @@ if __name__ == "__main__":
     LIST.insertHead(30)
     LIST.insertHead(20)
     LIST.insertHead(10)
-    print("\nBefore pop Operation\n")
-    LIST.PrintList()
-    print("\nAfter Pop Operation")
-    LIST.pop()
-    LIST.PrintList()
-    print(f"\n{LIST.__len__()}")
+    LIST.SearchIndex(5)
